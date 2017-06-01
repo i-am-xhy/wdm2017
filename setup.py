8265c163d2a53549b304897463bb9dcd1cde67f7
ncbakker <ncb21992@hotmail.com>
2017-06-01 13:24:26 +0200

    implemented sc1-sc5 for postgres, probably could use some further improvement

--- setup.py
+++ setup.py
@@ -1,24 +1,14 @@
 import os
 import falcon
+import json
 from api.code.apiserver import add_api_routes
 from waitress import serve
 
 WAITRESS_PORT = '5000'
 
-
-#class PeeweeConnectionMiddleware(object):
-    # def process_request(self, req, resp):
-    #     mysql_db.connect()
-    #
-    # def process_response(self, req, resp, resource):
-    #     if not mysql_db.is_closed():
-    #         mysql_db.close()
-
-
 if __name__ == '__main__':
-    app = falcon.API()#middleware=[PeeweeConnectionMiddleware()])
+    app = falcon.API()
     add_api_routes(app)
-    #add_chatbot_routes(app)
     if 'PORT' in os.environ:
         port = os.environ['PORT']
     else:
