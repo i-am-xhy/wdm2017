764e9d5905b86b64378aec7eda519ac42be7f5f7
Pawel Marciniak <paweueck@gmail.com>
2017-06-16 12:26:37 +0200

    bulk upload functionality to couch db
    bulk upload functionality to couch db

--- csv_to_couch_single_doc.py
+++ csv_to_couch_single_doc.py
@@ -37,54 +37,43 @@ def parseValue(value):
         except:
             pass 
     return value
-      
-couch = couchdb.Server('https://paweueck:admin@paweueck.cloudant.com/') #remote
-#couch = couchdb.Server('http://admin:admin@localhost:5984/') #local
+
+        
+#couch = couchdb.Server('https://paweueck:admin@paweueck.cloudant.com/') #remote
+couch = couchdb.Server('http://admin:admin@localhost:5984/') #local
 
 db = couch['movies'] # existing db
-
-
-
 alldocs = dict() #this will be used as register of file_name and documents we uploaded to the DB.
 
 
 #reading csv and converting each row to json i.e. python dictionary
 
 csv_folder = 'D:\Archive-temp\web_data_m\postgres_exported\\'
-file_name = "series"
+file_name = "genres"
 file_ext = ".csv"
 csv_path = csv_folder + file_name + file_ext
 
-#df = pandas.read_csv(csv_path, sep=';', header = 0, low_memory = False )
-
-#for series
-skiprows = [318362, 415098, 433542, 611610, 921684, 1030594, 1065826, 1188800]
-df = pandas.read_csv(csv_path, sep=';', header = 0, skiprows = skiprows )
-
-# for acted_in
-#skiprows = list(range(1,136000+640001)) + [2680652, 9244523, 10460573, 10794599, 12663641, 18002237, 21721094, 25082054]
-#df = pandas.read_csv(csv_path, sep=';', header = 0, skiprows = skiprows )
+df = pandas.read_csv(csv_path, sep=';', header = 0 )
 
 dflen = len(df)
 
-checkpoint = 20000 #this is how many records we will upload at once to the db
+checkpoint = 1000 #every n records, we will print the progress of writing to db
 
 newdocs = list()
-num_docs_uploaded = 0
 
 for x in range(dflen):
     newdoc = dict()
     for column in df.columns.values:
         value = parseValue(df[column][x])
         newdoc[column] = value
+        #newdoc = dict(newdoc)               
     newdoc['doctype'] = file_name #adding one key value representing the doctype - will be required later in couch for querying
+    db.save(newdoc)
+    
     newdocs.append(newdoc) # we are going to need this to keep track what we added to db - in case we want to remove what we added.
     
-    if ((x!=0) & (x%checkpoint==0))|(x==(dflen-1)):
-       db.update(newdocs)    
-       num_docs_uploaded+=len(newdocs)
-       print ("records written to couch: ", num_docs_uploaded, " / ", dflen) #for process completion indication
-       newdocs = list()
+    if x%checkpoint==0:
+       print ("records written to couch: ", x, " / ", dflen-1)
     #print(newdoc)
 
 #writing what we created to our register on the python side
@@ -93,6 +82,11 @@ alldocs[file_name] = newdocs
 
 #get docs from DB.
 
+
+# DELETION of documents - in case we want to get rid of them!
+#deletion of documents from db
+for item in newdocs:
+    db.delete(item)
 #deletion of document references from our register
 del alldocs[file_name]
 
