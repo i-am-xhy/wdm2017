343ac55f60e47bff1fae54bd385131fc796add6b
ncbakker <ncb21992@hotmail.com>
2017-06-20 16:57:17 +0200

    updated redis to have a more correct SC1,2 and 4

--- convertors/postgresql_to_redis.py
+++ convertors/postgresql_to_redis.py
@@ -6,6 +6,7 @@ import multiprocessing
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
+
 def set_data_for_actor(actor):
     r.hset('ACTOR:' + str(actor[0]), 'fname', actor[1])
     r.hset('ACTOR:' + str(actor[0]), 'lname', actor[2])
@@ -22,26 +23,41 @@ def set_data_for_movie(movie):
     r.hset('MOVIE:' + str(movie[0]), 'year', movie[2])
     r.hset('FMOVIE:' + str(movie[0]), 'title', movie[1])
     r.hset('FMOVIE:' + str(movie[0]), 'year', movie[2])
-
     r.sadd('MOVIESBYTITLE:' + str(movie[1]), movie[0])
+
 
 def set_data_for_movies_genre(mg):
     #r.sadd('MOVIESBYGENRE:'+str(mg[2]), mg[1])
     r.sadd('MOVIESBYGENREBYYEAR:'+str(mg[2]) + ':'+str(mg[3]), mg[1])
+    r.sadd('GENRESBYMOVIE:' + str(mg[1]), mg[2])
+
 
 def set_data_for_acted_in(ai):
     r.sadd('ACTEDIN:' + str(ai[1]), ai[3])
     r.sadd('HASACTORS:' + str(ai[3]), ai[1])
 
-def set_data_for_genre(g):
-    r.hset('GENRE:' + str(genre[0]), 'genre', genre[1])
-    r.sadd('GENRES', genre[0])
+    r.set('ROLEBYMOVIEBYACTOR:' + str(ai[1]) + ':' + str(ai[3]), ai[4])
+    #r.set('ROLE:' + ai[3], ai[4])
 
+
+
+def set_data_for_genre(g):
+    r.hset('GENRE:' + str(g[0]), 'genre', g[1])
+    r.sadd('GENRES', g[0])
+    r.set('GENRESBYNAME:' + str(g[1]), g[0])
+
+
+def set_data_for_series(s):
+    r.hset('FMOVIE:' + str(s[1]), 'name_of_series', s[2])
+
+
+def set_data_for_movies_keywords(mk):
+    r.sadd('KEYWORDSBYMOVIE:' + str(mk[1]), mk[5])
 
 if __name__ == '__main__':
     # for quick adaptability of the script add a limit to see if it works at all.
     # for a correct database transfer make this an empty string
-    potentialLimit = ''#'' LIMIT 10000'
+    potentialLimit = ' LIMIT 1000000'
 
     # todo refactor with apiserver.py to have common source
     conn = None
@@ -63,26 +79,33 @@ if __name__ == '__main__':
 
     # todo we probably don't want this to happen always
     r.flushdb()
-
+    # actors
     actors = execute('select a.idactors, a.fname, a.lname, a.gender, count(a.idactors) as moviecount from actors as a join acted_in as ai on a.idactors=ai.idactors group by a.idactors' + potentialLimit)
     Parallel(n_jobs=num_cores, verbose=5)(delayed(set_data_for_actor)(actor) for actor in actors)
-    print('done with actors')
 
+    # movies
     movies = execute('SELECT * FROM movies' + potentialLimit)
     Parallel(n_jobs=num_cores, verbose=5)(delayed(set_data_for_movie)(movie) for movie in movies)
-    print('done with movies')
 
-
+    # genres
     genres = execute('SELECT * FROM genres' + potentialLimit)
+    Parallel(n_jobs=num_cores, verbose=5)(delayed(set_data_for_genre)(genre) for genre in genres)
 
-    for genre in genres:
-        set_data_for_genre(genre)
-
+    # movies_genres
     movies_genres = execute('select mg.idmovies_genres, mg.idmovies, mg.idgenres, m.year  from movies_genres as mg join movies as m on mg.idmovies=m.idmovies' + potentialLimit)
     Parallel(n_jobs=num_cores, verbose=5)(delayed(set_data_for_movies_genre)(movies_genre) for movies_genre in movies_genres)
 
-    cur = conn.cursor('acted in nightmare cursor')
+    # acted_in
+    cur = conn.cursor('acted in nightmare cursor, store on server to prevent out of memory')
     cur.itersize = 100000
     cur.execute('select * from acted_in'+potentialLimit)
     Parallel(n_jobs=num_cores, verbose=5)(delayed(set_data_for_acted_in)(acted_in) for acted_in in cur)
+
+    # serie name
+    series = execute('SELECT * FROM series')
+    Parallel(n_jobs=num_cores, verbose=5)(delayed(set_data_for_series)(serie) for serie in series)
+
+    # keywords
+    movies_keywords = execute('SELECT * FROM movies_keywords AS mk JOIN keywords AS k ON mk.idkeywords=k.idkeywords')
+    Parallel(n_jobs=num_cores, verbose=5)(delayed(set_data_for_movies_keywords)(movie_keyword) for movie_keyword in movies_keywords)
 
